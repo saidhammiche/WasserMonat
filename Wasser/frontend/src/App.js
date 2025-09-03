acbe9e561e7ce1575aaf1f76e6833c729797a792
said hammiche <saidhammiche228@gmail.com>
2025-09-03 15:59:36 +0200

    Veroffentliche Version

--- Wasser/frontend/src/App.js
+++ Wasser/frontend/src/App.js
@@ -36,7 +36,7 @@ const AppWasserMonat = () => {
   const fetchData = useCallback(async () => {
     try {
       setLoadingAktualisieren(true);
-      const res = await axios.get("http://localhost:5000/api/wassermonat");
+      const res = await axios.get("http://4.245.3.186:5013/api/wassermonat");
       const allData = res.data;
       
       // Ajouter le nom du mois en fonction du numéro
@@ -119,7 +119,7 @@ const AppWasserMonat = () => {
       const start = Date.now();
 
       await axios.put(
-        `http://localhost:5000/api/wassermonat/${editingRecord.WasserMonatID}`,
+        `http://4.245.3.186:5013/api/wassermonat/${editingRecord.WasserMonatID}`,
         values
       );
 
